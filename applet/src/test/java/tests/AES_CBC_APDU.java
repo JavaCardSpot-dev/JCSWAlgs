2fa3af5a53239fa697238b86002fd25f15ceb7fe
arvind <arvindrao7589@gmail.com>
2018-04-18 22:44:14 +0200

    Added CBC mode example of AES, runs on hardware

--- applet/src/test/java/tests/AES_CBC_APDU.java
+++ applet/src/test/java/tests/AES_CBC_APDU.java
@@ -1,22 +1,8 @@
 package tests;
-
-import applets.Sha3Applet;
+import applets.AES_CBC_Applet;
 import cardTools.CardManager;
 import cardTools.RunConfig;
 import cardTools.Util;
-
-import javax.smartcardio.CardException;
-import javax.smartcardio.CommandAPDU;
-import javax.smartcardio.ResponseAPDU;
-import java.util.ArrayList;
-
-
-
-import applets.Sha3Applet;
-import cardTools.CardManager;
-import cardTools.RunConfig;
-import cardTools.Util;
-import com.google.common.primitives.Bytes;
 
 import javax.smartcardio.CardException;
 import javax.smartcardio.CommandAPDU;
@@ -27,25 +13,24 @@ import java.util.ArrayList;
  *
  * @author Petr Svenda petr@svenda.com
  */
-public class Sha3APDU {
+public class AES_CBC_APDU {
 
-    //private final static byte SELECT_TESTAPPLET[] = Util.hexStringToByteArray("5365637572654a617662");
-    private static byte APPLET_AID[] = Util.hexStringToByteArray("4C6162616B4170706C6578");
+    private static byte APPLET_AID[] = Util.hexStringToByteArray("4C6162616B4170706C6579");
     static short getShort(byte[] array, int offset) {
-        return (short) (((array[offset] & 0xFF) << 8) | (array[offset + 1] & 0xFF));
+        return (short) (((array[offset] & 0xFF) << 8) | (array[offset + 1] & 0xFF));        
     }
-
-    public static void main(String[] args) {
+    
+	 public static void main(String[] args) {
         try {
             demoSingleCommand();
         } catch (Exception ex) {
             System.out.println("Exception : " + ex.getMessage());
         }
     }
-
-
-
-    public static ResponseAPDU demoSingleCommand() throws Exception {
+	
+	
+	
+    public static ResponseAPDU  demoSingleCommand() throws Exception {
 
         // CardManager abstracts from real or simulated card, provide with applet AID
         final CardManager cardMngr = new CardManager(true, APPLET_AID);
@@ -57,11 +42,10 @@ public class Sha3APDU {
         runCfg.setTestCardType(RunConfig.CARD_TYPE.PHYSICAL); // Use real card
 
         // B) If running in the simulator
-        /*runCfg.setAppletToSimulate(Sha3Applet.class); // main class of applet to simulate
-        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL); // Use local simulator
+        /*runCfg.setAppletToSimulate(AES_CBC_Applet.class); // main class of applet to simulate
+       runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL); // Use local simulator
         runCfg.setbReuploadApplet(true);
-        runCfg.setInstallData(install_data);
-*/
+        runCfg.setInstallData(install_data);*/
         System.out.print("Connecting to card...");
         if (!cardMngr.Connect(runCfg)) {
             System.out.println(" Failed.");
@@ -70,9 +54,30 @@ public class Sha3APDU {
         ResponseAPDU response;
         // Connect to first available card
         // NOTE: selects target applet based on AID specified in CardManager constructor
-       System.out.println("Symmetric Block Ciphers: ");
+        System.out.println("Symmetric Block Ciphers: ");
         byte[] key;
         byte[] plain;
+
+
+        System.out.println("3)AES128");
+        System.out.println("Setting Key: ");
+        key=Util.hexStringToByteArray("00000000000000000000000000000000 ");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x11,0x23,0x32,key));
+        System.out.println("Encrypting: ");
+        plain=Util.hexStringToByteArray("F34481EC3CC627BACD5DC3FB08F273F34481EC3CC627BB000000000000000000F34481EC32");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x11,0x21,0x32,plain));
+        System.out.println("Decrypting: ");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x11,0x22,0x32,response.getData()));
+
+
+/*
+        System.out.println("RSA OAEP: ");
+        System.out.println("Encode: ");
+        plain=Util.hexStringToByteArray("11111122222211");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x25,0x51,0x00,plain));
+        System.out.println("Decode: ");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x25,0x52,0x00,response.getData()));
+
 
         System.out.println("Hash: ");
         plain=Util.hexStringToByteArray("00");  //only 50 bytes possible
@@ -94,13 +99,13 @@ public class Sha3APDU {
         response =cardMngr.transmit(new CommandAPDU(0x00,0x24,0x44,0x00,plain));
         System.out.println("KECCAK_r256c544: ");
         response =cardMngr.transmit(new CommandAPDU(0x00,0x24,0x46,0x00,plain));
-       // System.out.println("SHA512: ");
-       // response =cardMngr.transmit(new CommandAPDU(0x00,0x24,0x47,0x00,plain));
+        System.out.println("SHA512: ");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x24,0x47,0x00,plain));
 
-       // System.out.println("Tests: ");
-       // plain=Util.hexStringToByteArray("11111122222211");
-       // response =cardMngr.transmit(new CommandAPDU(0x00,0x23,0x00,0x00,plain));
-
+        System.out.println("Tests: ");
+        plain=Util.hexStringToByteArray("11111122222211");
+        response =cardMngr.transmit(new CommandAPDU(0x00,0x23,0x00,0x00,plain));
+*/
 
         return response;
 
@@ -118,7 +123,7 @@ public class Sha3APDU {
      * @return
      * @throws CardException
      */
-    public static ResponseAPDU sendCommandWithInitSequence(CardManager cardMngr, String command, ArrayList<String> initCommands) throws CardException {
+    public static ResponseAPDU sendCommandWithInitSequence(CardManager cardMngr, String command, ArrayList<String>  initCommands) throws CardException {
         if (initCommands != null) {
             for (String cmd : initCommands) {
                 cardMngr.getChannel().transmit(new CommandAPDU(Util.hexStringToByteArray(cmd)));
@@ -128,4 +133,4 @@ public class Sha3APDU {
         final ResponseAPDU resp = cardMngr.getChannel().transmit(new CommandAPDU(Util.hexStringToByteArray(command)));
         return resp;
     }
-}
+ }
